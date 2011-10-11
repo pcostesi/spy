02b9eeb662d8f65cec4ebd8af07046a2069ab0ea
Pablo Alejandro Costesich <pcostesi@labu51.unix.lab>
2011-10-11 14:02:20 -0300

    Added examples

---
+++ examples/2x.s
@@ -0,0 +1,7 @@
+IF X1 != 0 GOTO A1
+Z1 <- Z1 + 1
+IF Z1 != 0 GOTO E1
+[A1] Y <- Y + 1
+Y <- Y + 1
+X1 <- X1 - 1
+IF X1 != 0 GOTO A1
---
+++ examples/div_2.s
@@ -0,0 +1,8 @@
+[A1] X1 <- X1 - 1
+IF X1 != 0 GOTO B1
+Z1 <- Z1 + 1
+IF Z1 != 0 GOTO E1
+[B1] X1 <- X1 - 1
+Y <- Y + 1
+IF X1 != 0 GOTO A1
+
