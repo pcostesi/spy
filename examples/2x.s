8b8b39569205f7f7904cd2d9e704a1cd64daf990
Pablo Alejandro Costesich <rhfixer@gmail.com>
2012-02-07 15:08:17 -0300

    Fixed examples so they comply with the new grammar

--- examples/2x.s
+++ examples/2x.s
@@ -1,7 +1,7 @@
-IF X1 != 0 GOTO A1
+if X1 != 0 goto A1
 Z1 <- Z1 + 1
-IF Z1 != 0 GOTO E1
+if Z1 != 0 goto E1
 [A1] Y <- Y + 1
 Y <- Y + 1
 X1 <- X1 - 1
-IF X1 != 0 GOTO A1
+if X1 != 0 goto A1
--- examples/div_2.s
+++ examples/div_2.s
@@ -1,8 +1,8 @@
 [A1] X1 <- X1 - 1
-IF X1 != 0 GOTO B1
+if X1 != 0 goto B1
 Z1 <- Z1 + 1
-IF Z1 != 0 GOTO E1
+if Z1 != 0 goto E1
 [B1] X1 <- X1 - 1
 Y <- Y + 1
-IF X1 != 0 GOTO A1
+if X1 != 0 goto A1
 
