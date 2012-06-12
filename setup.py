24ce6c4a6aae9302cd4929b2972f694803edc506
Pablo Alejandro Costesich <rhfixer@gmail.com>
2012-06-12 14:58:08 -0300

    Updated to 0.3.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import Command, setup
 
 setup(
     name='spy',
-    version='0.3.2',
+    version='0.3.5',
     url='http://github.com/pcostesi/spy/',
     license='BSD',
     author='Pablo Alejandro Costesich',
